6b2750a5ff72d540a36ce470e3ca259ed70d4b92
anyang <anyang@sephora.cn>
2021-07-01 19:02:33 +0800

    update schedule

--- src/main/java/cn/family/aliddns/schedule/DomainRecordTask.java
+++ src/main/java/cn/family/aliddns/schedule/DomainRecordTask.java
@@ -1,6 +1,8 @@
 package cn.family.aliddns.schedule;
 
+import cn.family.aliddns.service.IAliYunDDnsService;
 import cn.family.aliddns.utils.DateUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -13,8 +15,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class DomainRecordTask {
 
-    @Scheduled(cron = "0 0/1 * * * ?")
+    @Autowired
+    private IAliYunDDnsService aliYunDDnsService;
+
+
+    @Scheduled(cron = "0 0/15 * * * ?")
     public void execute() {
+        aliYunDDnsService.manageDescribeDomainRecords();
         System.out.println("执行了一次" + DateUtils.getCurrentSytemTimeForString());
     }
 }
